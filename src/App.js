32fb941fcbcb38f61db379d03c86a40432ecdd70
Izak Uy <s3948246@student.rmit.edu.au>
2023-08-02 19:02:26 +1000

    added font, slide card element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './pages/header'
 import Navbar from './pages/navbar'
+import Slide from './pages/slide'
 
 function App() {
   return (
     <div>
       <Header />
       <Navbar />
+      <Slide />
     </div>
   );
 }
---
+++ src/pages/slide.js
@@ -0,0 +1,11 @@
+import './slide.css'
+
+const slide =()=> {
+    return (
+        <div className='slide-wrapper'>
+            <img className='slide-image' src='https://www.hollywoodreporter.com/wp-content/uploads/2022/07/Oppenheimer-Movie-Poster-Universal-Publicity-EMBED-2022-.jpg?w=1000'></img>
+        </div>
+    )
+}
+
+export default slide
