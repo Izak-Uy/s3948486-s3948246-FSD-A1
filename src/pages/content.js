28220cbcf068c6d7d58764aba09ab8d46bd878b0
Izak Uy <s3948246@student.rmit.edu.au>
2023-08-09 13:35:15 +1000

    further homepage edits

--- src/pages/content.js
+++ src/pages/content.js
@@ -2,7 +2,20 @@ import './content.css'
 
 const content =()=> {
     return (
-        <div>thing</div>
+        <div className='content-wrapper'>
+            <div className='content-header'> New Releases</div>
+            <div className='content-container'>
+                <div className='left-content-col'>
+                    <div className='movie-slot-container'>
+                        this
+                    </div>
+                </div>
+                <div className='right-content-col'>
+                    More movies go here
+                </div>
+            </div>
+
+        </div>
     )
 }
 
