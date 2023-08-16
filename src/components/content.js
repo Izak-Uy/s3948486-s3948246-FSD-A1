6f0dcb756aa84d2aaa4ca81d9513c2453cac8d00
Izak Uy <s3948246@student.rmit.edu.au>
2023-08-16 19:20:07 +1000

    more homepgae stuff

--- src/components/content.js
+++ src/components/content.js
@@ -6,7 +6,7 @@ const Content =({imageArrayProps})=> {
 
     return (
         <div className='content-wrapper'>
-            <section className='releases'>
+            <div className='releases'>
                 <div className='content-header'> New Releases</div>
                 {/* {imageArray.map((MovImg, index) => ( */}
                     <div className='content-container'>
@@ -33,10 +33,20 @@ const Content =({imageArrayProps})=> {
                         </div>
                     </div>
                 {/* ))} */}
-            </section>
-            <section className='about-us'>
-                dayum loop cinema goated
-            </section>
+            </div>
+            <div className='about-us'>
+                <div className='about-us-text'>
+                    <h1>Our Cinemas</h1>
+                    <div className='p-text'>
+                        <p>
+                            Loop Cinema goes by the community known saying: We movies. We strive everyday to provide to our community with stellar new movie releases as well as old but gold cinema classics. We work hard with our community representatives to give everybody a welcoming and enjoyable cinema experience perfect for solo goers, romantic date nights and even family fun. No other cinema can even come close to match our level of dedication we give to our community and all we ask for return is your money.
+                        </p>
+                        <p>
+                            We have cinemas all across Melbourne, ranging from Warrnambool to Flagstaff to the city. You can be sure that around the corner we will be there. We are an inevitable and enjoyable experience. 
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,7 @@ function Home() {
     "https://www.hollywoodreporter.com/wp-content/uploads/2022/07/Oppenheimer-Movie-Poster-Universal-Publicity-EMBED-2022-.jpg?w=1000",
     "https://variety.com/wp-content/uploads/2023/04/Fs3zzy_aMAAyCNC.jpeg?w=800",
     "https://m.media-amazon.com/images/I/61FsQdm0-ML._AC_UF894,1000_QL80_.jpg",
+    "https://m.media-amazon.com/images/M/MV5BN2YyZjQ0NTEtNzU5MS00NGZkLTg0MTEtYzJmMWY3MWRhZjM2XkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_FMjpg_UX1000_.jpg",
   ];
   
   return (
