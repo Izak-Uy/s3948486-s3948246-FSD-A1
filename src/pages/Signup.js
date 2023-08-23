dbc7ba5faa79fece0040bf3f2843f1fe16c8f3e5
SeanT-RMIT <s3948486@student.rmit.edu.au>
2023-08-23 18:24:34 +1000

    changed hidden top element to p

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,15 @@
 import './Signup.css'
 import Navbar from "../components/navbar";
-
+import useForm from "../hooks/useForm"
 
 
 function Signup() {
+  const { values, handleChange, handleSubmit } = useForm(signup);
+
+  function signup() {
+    console.log(values);
+  }
+
   return (
     <div>
       <Navbar scrollTop={false}/>
@@ -12,25 +18,25 @@ function Signup() {
           <h1>Sign Up</h1>
         </div>
         <div className="signup-form">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-email">
               <label className="field-title">
                 Email:
                 <br />
-                <input type="email" placeholder="Enter email" />
+                <input type="email" placeholder="Enter email" onChange={handleChange} value={values.email} required />
               </label>
             </div>
             <div className="form-password">
               <label className="field-title">
                 Password:
                 <br />
-                <input type="password" placeholder="Enter password" />
+                <input type="password" placeholder="Enter password" onChange={handleChange} value={values.password} required />
               </label>
               <br />
               <label className="field-title">
                 Confirm Password:
                 <br />
-                <input type="password" placeholder="Enter password" />
+                <input type="password" placeholder="Enter password" onChange={handleChange} value={values.passwordConfirm} required />
               </label>
             </div>
             <input type="submit" value="Submit" />
